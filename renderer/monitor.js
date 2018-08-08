b522c600e28293da056fd300b970d7590c65271d
Felipe <felipeballesteros1@gmail.com>
2018-08-07 20:41:13 -0700

    Updated sidebar functionality

--- renderer/monitor.js
+++ renderer/monitor.js
@@ -74,3 +74,20 @@ var data = {
   })
 
   fetchData()
+
+  var acc = document.getElementById("sensors-button");
+
+  
+    acc.addEventListener("click", function() {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("is-active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
