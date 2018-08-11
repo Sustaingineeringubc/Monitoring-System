af0f4b133352e93b0e0d48eb775e217a483dcf23
Adrian Viquez <adrianviquez@Adrians-MacBook-Pro-2.local>
2018-08-11 10:13:33 -0700

    signup page implementation

--- renderer/signup.js
+++ renderer/signup.js
@@ -5,14 +5,45 @@ $('#signup-button').click(() => {
     let email = $('#email').val();
     
     if (!email || !password) {
+        emailCheckEmpty(email);
+        passwordCheckEmpty(password);
         return
     }
 
-  ipcRenderer.send('is-new-user', {password, email})   
-  })
+ipcRenderer.send('is-new-user', {password, email})   
+})
 
-  ipcRenderer.on('is-new-user', (e, msg) => {
-    if (msg.error) {
-        alert( msg.error)
+var emailCheckEmpty = function (email) {
+    if(!email) {
+        $('#email').addClass("is-danger");
+        $('#email').removeClass("is-primary");
+        $('#emailCheckbox').addClass("hidden");
+    } else {
+        $('#email').addClass("is-primary");
+        $('#email').removeClass("is-danger");
+        $('#emailCheckbox').removeClass("hidden");
     }
-  })
+}
+
+var passwordCheckEmpty = function (password) {
+    if(!password) {
+        $('#password').addClass("is-danger");
+        $('#password').removeClass("is-primary");
+    } else {
+        $('#password').removeClass("is-danger");
+        $('#password').addClass("is-primary");
+    }
+}
+
+ipcRenderer.on('is-new-user', (e, msg) => {
+if (msg.error) {
+    alert( msg.error)
+}
+})
+
+var signupTitle = $("#signupTitle");
+var signupSubtitle = $("#signupSubtitle");
+var box = $("#box");
+signupTitle.slideToggle("slow");
+signupSubtitle.slideToggle("slow");
+box.slideToggle("slow");
