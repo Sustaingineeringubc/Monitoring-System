836f5fdd968c988e7c586ec4f6067777603c0fbd
Israel Trujillo Quintero <israeltrujilloq96@gmail.com>
2018-09-29 20:58:14 -0700

    Preliminary Verify Email Screen

---
+++ renderer/verifyEmail.js
@@ -0,0 +1,51 @@
+const {ipcRenderer} = require('electron')
+
+// Verify Email
+$('#verifyEmail-button').click(() => {
+    SendForm()
+});
+
+// Enter-Key Functionality
+$("#email").keypress(function(event) {
+    SendForm(event) 
+})
+
+var emailCheckEmpty = function (email) {
+    if(!email) {
+        $('#email').addClass("is-danger");
+        $('#email').removeClass("is-primary");
+        $('#emailCheckbox').addClass("hidden");
+    } else {
+        $('#email').addClass("is-primary");
+        $('#email').removeClass("is-danger");
+        $('#emailCheckbox').removeClass("hidden");
+    }
+}
+
+ipcRenderer.on('email-exists', (e, msg) => {
+    if (msg.error) {
+        alert( msg.error)
+    }
+})
+
+function SendForm(event) {
+    if(event){
+        var key = event.which;
+        if (key !== 13){
+            return
+        }
+    }
+
+    let email = $('#email').val();
+    let isRemembered = $('#remember').prop('checked');
+
+    // Looses focus effect when signup button is pressed
+    $('#email').blur();
+
+    if (!email) {
+        emailCheckEmpty(email);
+        return
+    }
+
+    ipcRenderer.send('email-exists', {email, isRemembered}) 
+}
